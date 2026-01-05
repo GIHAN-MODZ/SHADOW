01f46a0ddf43ccb801ae65e4bb604ecbd735886f
GIHAN MIHIRANGA <212064412+GIHAN-MODZ@users.noreply.github.com>
2026-01-05 09:22:00 +0530

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,19 @@
+//================================//
+// Main
+//================================//
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['94776477438']
+//================================//
+// Sticker
+//================================//
+global.packname = "Sticker By RizxVelz"
+global.author = "RizxvelzOfficial"
+//================================//
+// Global Mess
+//================================//
+global.menuMode = global.menuMode || 'button';
+global.mess = {
+ owner: '*You are not the owner*',
+ premium: '*You are not premium*',
+ group: '*This feature is for groups only*'
+}
